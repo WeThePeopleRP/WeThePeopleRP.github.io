8db48f7a1cbe2fefed10fffe4036cb4f53c6a08d
AmIMuted <r.acaldwell@outlook.com>
2024-06-16 20:12:17 -0700

    update

--- script.js
+++ script.js
@@ -1,32 +1,8 @@
 document.addEventListener("DOMContentLoaded", function() {
-    var myVideo = document.getElementById("video1");
-
-    document.querySelector("button[onclick='playPause()']").addEventListener("click", playPause);
-    document.querySelector("button[onclick='makeBig()']").addEventListener("click", makeBig);
-    document.querySelector("button[onclick='makeSmall()']").addEventListener("click", makeSmall);
-    document.querySelector("button[onclick='makeNormal()']").addEventListener("click", makeNormal);
-
-    function playPause() {
-        if (myVideo.paused) {
-            myVideo.play();
-        } else {
-            myVideo.pause();
-        }
+    if (!window.location.hash) {
+        document.body.classList.remove('fade-out');
     }
 
-    function makeBig() {
-        myVideo.width = 560;
-    }
-
-    function makeSmall() {
-        myVideo.width = 320;
-    }
-
-    function makeNormal() {
-        myVideo.width = 420;
-    }
-
-    // For smooth fade-out transition on navigation
     document.querySelectorAll('a').forEach(link => {
         if (link.hostname === window.location.hostname) {
             link.addEventListener('click', function(event) {
